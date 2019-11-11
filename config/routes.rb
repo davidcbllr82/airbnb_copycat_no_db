38fb0a7043cc26df2e5473addc0fc06fdcc88d6d
David Caballero <david.caballero@code.berlin>
2019-11-11 19:48:27 +0100

    Routes working. Needs front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    get 'flats/:id', to: 'flats#show', as: :flat
+    root to: 'flats#index'
+  end
 end
